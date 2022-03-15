a16f9b15a93dcf861079dab07e0b24ed584353c9
Andrew Manderson <a.manderson@live.co.uk>
2022-03-15 16:19:38 +0000

    cv initial commit

--- lua/strong.lua
+++ lua/strong.lua
@@ -2,15 +2,20 @@ function Block(el)
     if el.t == "Para" or el.t == "Plain" then
         for k, _ in ipairs(el.content) do
 
-            if el.content[k].t == "Str" and el.content[k].text == "Zhang," and
+            if el.content[k].t == "Str" and el.content[k].text == "Manderson," and
                 el.content[k + 1].t == "Space" and el.content[k + 2].t == "Str" and
-                el.content[k + 2].text:find("^H.") then
+                el.content[k + 2].text:find("^A.") and
+                el.content[k + 3].t == "Space" and el.content[k + 4].t == "Str" and
+                el.content[k + 4].text:find("^A.")
+                then
 
-                local _, e = el.content[k + 2].text:find("^H.")
+                print(el.content[k + 1].text, el.content[k + 2].text, el.content[k + 3].text, el.content[k + 4].text)
+                local _, e = el.content[k + 2].text:find("^A.")
                 local rest = el.content[k + 2].text:sub(e + 1) -- empty if e+1>length
-                el.content[k] = pandoc.Strong {pandoc.Str("Zhang, H.")}
+                el.content[k] = pandoc.Strong {pandoc.Str("Manderson, A. A.,")}
                 el.content[k + 1] = pandoc.Str(rest)
                 table.remove(el.content, k + 2) -- safe? another way would be to set element k+2 to Str("")
+                table.remove(el.content, k + 3)
                 -- no real need to skip ipairs items here
 
             end
