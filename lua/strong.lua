7c82a03575ce193531e1f9ad465890332f0538dc
Andrew Manderson <a.manderson@live.co.uk>
2022-03-15 17:19:35 +0000

    capitalisation and other tweaks

--- lua/strong.lua
+++ lua/strong.lua
@@ -8,14 +8,16 @@ function Block(el)
                 el.content[k + 3].t == "Space" and el.content[k + 4].t == "Str" and
                 el.content[k + 4].text:find("^A.")
                 then
-
-                print(el.content[k + 1].text, el.content[k + 2].text, el.content[k + 3].text, el.content[k + 4].text)
+                -- Original approach assumes that you only have one initial, this approach assumes you have two.
+                -- The pattern should(?) generalise if you have more.
+                -- Uncomment the below line to see the structure at and around the important index of el.content
+                -- print(el.content[k + 1].text, el.content[k + 2].text, el.content[k + 3].text, el.content[k + 4].text)
                 local _, e = el.content[k + 2].text:find("^A.")
                 local rest = el.content[k + 2].text:sub(e + 1) -- empty if e+1>length
                 el.content[k] = pandoc.Strong {pandoc.Str("Manderson, A. A.,")}
                 el.content[k + 1] = pandoc.Str(rest)
                 table.remove(el.content, k + 2) -- safe? another way would be to set element k+2 to Str("")
-                table.remove(el.content, k + 3)
+                table.remove(el.content, k + 3) -- We've merged the two initials, which used to be separate entries, into one entry, so we must now delete the original second initial
                 -- no real need to skip ipairs items here
 
             end
